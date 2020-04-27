e1e5a849c2e511778a3d2c9830db4b1dc4f699de
tabufellin <dsgspersonal@gmail.com>
2020-04-27 00:14:02 -0600

    cambios a splaytree para que funcione y par ahacer pruebas

--- SplayTree.java
+++ SplayTree.java
@@ -2,39 +2,49 @@
  *  Java Program to Implement SplayTree
  **/
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /** Class Node **/
-class SplayNode
+class SplayNode <S>
 {
     SplayNode left, right, parent;
-    int element;
+
+    S elementInEnglish;
+    S elementInSpanish;
+
 
     /** Constructor **/
     public SplayNode()
     {
-        this(0, null, null, null);
+        this.left= null;
+        this.right=null;
+        this.parent=null;
+        this.parent=null;
+        this.elementInEnglish=null;
+        this.elementInSpanish=null;
     }
     /** Constructor **/
-    public SplayNode(int ele)
+    public SplayNode(S elementInEnglish, S elementInSpanish)
     {
-        this(ele, null, null, null);
-    }
-    /** Constructor **/
-    public SplayNode(int ele, SplayNode left, SplayNode right, SplayNode parent)
-    {
-        this.left = left;
-        this.right = right;
-        this.parent = parent;
-        this.element = ele;
+        this.left = null;
+        this.right = null;
+        this.parent = null;
+        this.elementInEnglish = elementInEnglish;
+        this.elementInSpanish = elementInSpanish;
     }
 }
 
+
+
+
+
 /** Class SplayTree **/
-class SplayTree
+class SplayTree <S>
 {
     private SplayNode root;
     private int count = 0;
+    ArrayList<SplayNode> splayList = new ArrayList<>();
 
     /** Constructor **/
     public SplayTree()
@@ -42,84 +52,25 @@ class SplayTree
         root = null;
     }
 
-    /** Function to check if tree is empty **/
-    public boolean isEmpty()
-    {
-        return root == null;
-    }
-
-    /** clear tree **/
-    public void clear()
-    {
-        root = null;
-        count = 0;
-    }
 
     /** function to insert element */
-    public void insert(int ele)
+    public void insert(SplayNode<S> element)
     {
         SplayNode z = root;
         SplayNode p = null;
         while (z != null)
         {
             p = z;
-            if (ele > p.element)
+            if (element.elementInEnglish == p.elementInEnglish)
                 z = z.right;
             else
                 z = z.left;
         }
-        z = new SplayNode();
-        z.element = ele;
-        z.parent = p;
-        if (p == null)
-            root = z;
-        else if (ele > p.element)
-            p.right = z;
-        else
-            p.left = z;
+
+        assert false;
         Splay(z);
+
         count++;
-    }
-    /** rotate **/
-    public void makeLeftChildParent(SplayNode c, SplayNode p)
-    {
-        if ((c == null) || (p == null) || (p.left != c) || (c.parent != p))
-            throw new RuntimeException("WRONG");
-
-        if (p.parent != null)
-        {
-            if (p == p.parent.left)
-                p.parent.left = c;
-            else
-                p.parent.right = c;
-        }
-        if (c.right != null)
-            c.right.parent = p;
-
-        c.parent = p.parent;
-        p.parent = c;
-        p.left = c.right;
-        c.right = p;
-    }
-
-    /** rotate **/
-    public void makeRightChildParent(SplayNode c, SplayNode p)
-    {
-        if ((c == null) || (p == null) || (p.right != c) || (c.parent != p))
-            throw new RuntimeException("WRONG");
-        if (p.parent != null)
-        {
-            if (p == p.parent.left)
-                p.parent.left = c;
-            else
-                p.parent.right = c;
-        }
-        if (c.left != null)
-            c.left.parent = p;
-        c.parent = p.parent;
-        p.parent = c;
-        p.right = c.left;
-        c.left = p;
     }
 
     /** function splay **/
@@ -169,132 +120,82 @@ class SplayTree
         root = x;
     }
 
-    /** function to remove element **/
-    public void remove(int ele)
+    /** rotate **/
+    public void makeLeftChildParent(SplayNode c, SplayNode p)
     {
-        SplayNode node = findNode(ele);
-        remove(node);
+        if ((c == null) || (p == null) || (p.left != c) || (c.parent != p))
+            throw new RuntimeException("WRONG");
+
+        if (p.parent != null)
+        {
+            if (p == p.parent.left)
+                p.parent.left = c;
+            else
+                p.parent.right = c;
+        }
+        if (c.right != null)
+            c.right.parent = p;
+
+        c.parent = p.parent;
+        p.parent = c;
+        p.left = c.right;
+        c.right = p;
     }
 
-    /** function to remove node **/
-    private void remove(SplayNode node)
+    /** rotate **/
+    public void makeRightChildParent(SplayNode c, SplayNode p)
     {
-        if (node == null)
-            return;
-
-        Splay(node);
-        if( (node.left != null) && (node.right !=null))
+        if ((c == null) || (p == null) || (p.right != c) || (c.parent != p))
+            throw new RuntimeException("WRONG");
+        if (p.parent != null)
         {
-            SplayNode min = node.left;
-            while(min.right!=null)
-                min = min.right;
-
-            min.right = node.right;
-            node.right.parent = min;
-            node.left.parent = null;
-            root = node.left;
+            if (p == p.parent.left)
+                p.parent.left = c;
+            else
+                p.parent.right = c;
         }
-        else if (node.right != null)
-        {
-            node.right.parent = null;
-            root = node.right;
-        }
-        else if( node.left !=null)
-        {
-            node.left.parent = null;
-            root = node.left;
-        }
-        else
-        {
-            root = null;
-        }
-        node.parent = null;
-        node.left = null;
-        node.right = null;
-        node = null;
-        count--;
+        if (c.left != null)
+            c.left.parent = p;
+        c.parent = p.parent;
+        p.parent = c;
+        p.right = c.left;
+        c.left = p;
     }
 
-    /** Functions to count number of nodes **/
-    public int countNodes()
-    {
-        return count;
-    }
 
-    /** Functions to search for an element **/
-    public boolean search(int val)
-    {
-        return findNode(val) != null;
-    }
 
-    private SplayNode findNode(int ele)
-    {
-        SplayNode PrevNode = null;
-        SplayNode z = root;
-        while (z != null)
-        {
-            PrevNode = z;
-            if (ele > z.element)
-                z = z.right;
-            else if (ele < z.element)
-                z = z.left;
-            else if(ele == z.element) {
-                Splay(z);
-                return z;
-            }
 
-        }
-        if(PrevNode != null)
-        {
-            Splay(PrevNode);
-            return null;
-        }
-        return null;
-    }
 
-    /** Function for inorder traversal **/
-    public void inorder()
-    {
+
+    public void inorder() {
         inorder(root);
     }
-    private void inorder(SplayNode r)
-    {
-        if (r != null)
-        {
+
+    private void inorder(SplayNode r) {
+
+        if (r != null) {
             inorder(r.left);
-            System.out.print(r.element +" ");
+            splayList.add(r);
             inorder(r.right);
         }
+
     }
 
-    /** Function for preorder traversal **/
-    public void preorder()
+
+    /** Functions to search for an element **/
+    public String search(String word)
     {
-        preorder(root);
-    }
-    private void preorder(SplayNode r)
-    {
-        if (r != null)
-        {
-            System.out.print(r.element +" ");
-            preorder(r.left);
-            preorder(r.right);
+        inorder();
+        for (SplayNode splayNode : splayList) {
+            if (splayNode.elementInEnglish.equals(word)) {
+                // System.out.println();
+                //  System.out.println(splayList.get(i).spanish);
+                return (String) splayNode.elementInSpanish;
+            }
         }
+        return "*"+word+"*";
     }
 
-    /** Function for postorder traversal **/
-    public void postorder()
-    {
-        postorder(root);
-    }
-    private void postorder(SplayNode r)
-    {
-        if (r != null)
-        {
-            postorder(r.left);
-            postorder(r.right);
-            System.out.print(r.element +" ");
-        }
-    }
+
 }
 
